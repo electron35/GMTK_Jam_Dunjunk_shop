08c4e5966d4d8c27739e25aa67f8b165a7efcf84
electron35 <eleque.turone@outlook.com>
2023-07-08 09:36:43 +0200

    Change Folder Name

--- GMTK_Jam/script/MoneyUI.gd
+++ GMTK_Jam/script/MoneyUI.gd
@@ -1,12 +1,15 @@
-extends HSplitContainer
+extends TextureRect
+
+@export_range(1, 100) var wait_time:float = 5
 
 func _init():
+	$SplitBox/Timer/Time.wait_time = wait_time
 	pass
 	
 func _process(delta):
 	Main.money += 1
-	$Timer.text = str(int($Timer/Time.time_left)) + "s"
-	$Money.text = str(Main.money)
+	$SplitBox/Timer.text = str(int($SplitBox/Timer/Time.time_left)) + "s"
+	$SplitBox/Money.text = str(Main.money)
 
 func _on_time_timeout():
 	Main.pay_rent()
