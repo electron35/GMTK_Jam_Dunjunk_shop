e5780d1c5818ae68319e511ae2945c1a4298496b
pierre bigaud <pierrebigaud44@orange.fr>
2023-07-09 10:35:04 +0200

    MenuPrincipalEtTuto

--- GMTK_Jam/script/menuScene.gd
+++ GMTK_Jam/script/menuScene.gd
@@ -5,3 +5,37 @@ func _ready():
 	$Musique.playing=true;
 	pass # Replace with function body.
 
+
+
+func _on_jouer_pressed():
+	$ButtonSoundMage.play()
+	await $ButtonSoundMage.finished
+	$Musique.stop()
+	$MarginContainer.show()
+	pass # Replace with function body.
+
+
+func _on_credits_pressed():
+	$ButtonSoundRogue.play()
+	$creditMap.show()
+	pass # Replace with function body.
+
+
+func _on_quitter_pressed():
+	$ButtonSoundFight.play()
+	await $ButtonSoundFight.finished
+	get_tree().quit()
+	pass # Replace with function body.
+
+
+func _on_retour_pressed():
+	$ButtonSoundRogue.play()
+	$creditMap.hide()
+	pass # Replace with function body.
+
+
+func _on_continue_pressed():
+	$ButtonSoundFight.play()
+	await $ButtonSoundFight.finished
+	get_tree().change_scene_to_file("res://GMTK_Jam/MainScene.tscn")
+	pass # Replace with function body.
